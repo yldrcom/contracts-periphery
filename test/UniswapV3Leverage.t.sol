b1e8ef092820bae5bcb3b91bdcf5c11f6d5a1cd6
Arsenii Kulikov <klkvrr@gmail.com>
2024-01-26 13:32:13 +0200

    Add test

--- test/UniswapV3Leverage.t.sol
+++ test/UniswapV3Leverage.t.sol
@@ -290,6 +290,54 @@ contract UniswapV3LeverageTest is BaseTest, IUniswapV3SwapCallback {
         assertGt(liquidityAfter, liquidityBefore);
     }
 
+    function test_deleverage_fees() public {
+        (uint256 tokenId,,) = uniswapV3Testing.acquireUniswapPosition(
+            address(usdc), address(weth), 2000e6, 1e18, UniswapV3Testing.PositionType.Both
+        );
+
+        (,,,,,,, uint128 liquidityBefore,,,,) = uniswapV3Testing.positionManager.positions(tokenId);
+
+        UniswapV3LeveragedPosition.PositionInitParams memory params = UniswapV3LeveragedPosition.PositionInitParams({
+            tokenId: tokenId,
+            tokenToBorrow: address(usdc),
+            amountToBorrow: 1000e6,
+            flashLoanProvider: aaveFlashloan,
+            assetConverter: assetConverter,
+            owner: ALICE,
+            maxSwapSlippage: 50
+        });
+
+        uniswapV3Testing.positionManager.safeTransferFrom(
+            ALICE, address(uniswapV3Leverage), tokenId, abi.encode(params)
+        );
+
+        UniswapV3LeveragedPosition position =
+            UniswapV3LeveragedPosition(StdUtils.computeCreateAddress(address(uniswapV3Leverage), 2));
+
+        (uint160 currentSqrtPrice,,,,,,) = position.uniswapV3Pool().slot0();
+        vm.stopPrank();
+        // Do some movements to increase fees
+        uniswapV3Testing.movePoolPrice(address(usdc), address(weth), 500, currentSqrtPrice * 101 / 100);
+        uniswapV3Testing.movePoolPrice(address(usdc), address(weth), 500, currentSqrtPrice);
+
+        (uint256 fees0Before, uint256 fees1Before) = uniswapV3Wrapper.getPendingFees(tokenId);
+
+        vm.startPrank(ALICE);
+        position.deleverage(
+            aaveFlashloan,
+            UniswapV3LeveragedPosition.DeleverageParams({
+                assetConverter: assetConverter,
+                receiver: ALICE,
+                maxSwapSlippage: 50
+            })
+        );
+
+        (,,,,,,,,,, uint128 tokensOwed0, uint128 tokensOwed1) = uniswapV3Testing.positionManager.positions(tokenId);
+
+        assertEq(tokensOwed0, fees0Before);
+        assertEq(tokensOwed1, fees1Before);
+    }
+
     function uniswapV3SwapCallback(int256 amount0Delta, int256 amount1Delta, bytes calldata /* data */ ) external {
         address token0 = IUniswapV3Pool(msg.sender).token0();
         address token1 = IUniswapV3Pool(msg.sender).token1();
