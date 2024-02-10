cd9934bc59f1791f458e76affb4d5210ba29d9e5
Arsenii Kulikov <klkvrr@gmail.com>
2024-02-10 03:29:28 +0300

    yldr and combined flashloan

---
+++ src/flashloan/CombinedERC3156Wrapper.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {IERC3156FlashLender, IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
+import {IPool} from "@aave-v3/core/contracts/interfaces/IPool.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract CombinedERC3156Wrapper is IERC3156FlashLender, IERC3156FlashBorrower {
+    using SafeERC20 for IERC20;
+
+    IERC3156FlashLender public immutable mainLender;
+    IERC3156FlashLender public immutable fallbackLender;
+
+    /// @notice Common data for flashloan chain
+    /// @param receiver - receiver of the starting flashloan
+    /// @param data - data for the receiver
+    /// @param amount - the total amount receiver requested
+    struct CommonData {
+        IERC3156FlashBorrower receiver;
+        address initiator;
+        bytes data;
+        uint256 amount;
+    }
+
+    /// @notice Wrapper around `CommonData` for callback from main lender
+    /// @param amountFromCallback - amount we need to request from fallback lender (0 if don't need any)
+    struct MainCallbackData {
+        CommonData commonData;
+        uint256 amountFromFallback;
+    }
+
+    /// @notice Wrapper around `CommonData` for callback from fallback lender
+    /// @param mainFee - fee that we need to pay to main lender
+    struct FallbackCallbackData {
+        CommonData commonData;
+        uint256 mainFee;
+    }
+
+    constructor(IERC3156FlashLender _mainLender, IERC3156FlashLender _fallbackLender) {
+        mainLender = _mainLender;
+        fallbackLender = _fallbackLender;
+    }
+
+    /// @inheritdoc IERC3156FlashLender
+    function maxFlashLoan(address token) external view override returns (uint256) {
+        return mainLender.maxFlashLoan(token) + fallbackLender.maxFlashLoan(token);
+    }
+
+    /// @inheritdoc IERC3156FlashLender
+    function flashFee(address token, uint256 amount) external view override returns (uint256) {
+        uint256 maxMain = mainLender.maxFlashLoan(token);
+
+        if (amount <= maxMain) {
+            return mainLender.flashFee(token, amount);
+        } else {
+            return mainLender.flashFee(token, maxMain) + fallbackLender.flashFee(token, amount - maxMain);
+        }
+    }
+
+    /// @inheritdoc IERC3156FlashLender
+    function flashLoan(IERC3156FlashBorrower receiver, address token, uint256 amount, bytes calldata userData)
+        external
+        override
+        returns (bool)
+    {
+        uint256 maxMain = mainLender.maxFlashLoan(token);
+
+        MainCallbackData memory callbackData = MainCallbackData({
+            commonData: CommonData({receiver: receiver, initiator: msg.sender, data: userData, amount: amount}),
+            amountFromFallback: amount > maxMain ? amount - maxMain : 0
+        });
+
+        bytes memory data = abi.encode(callbackData);
+        mainLender.flashLoan(this, token, amount - callbackData.amountFromFallback, data);
+        return true;
+    }
+
+    function _issueFlashloan(address token, CommonData memory commonData, uint256 totalFee) internal {
+        IERC20(token).safeTransfer(address(commonData.receiver), commonData.amount);
+        require(
+            commonData.receiver.onFlashLoan(commonData.initiator, token, commonData.amount, totalFee, commonData.data)
+                == keccak256("ERC3156FlashBorrower.onFlashLoan"),
+            "IERC3156: Callback failed"
+        );
+        IERC20(token).safeTransferFrom(address(commonData.receiver), address(this), commonData.amount + totalFee);
+    }
+
+    function onFlashLoan(address initiator, address token, uint256 amount, uint256 fee, bytes calldata params)
+        external
+        returns (bytes32)
+    {
+        require(initiator == address(this), "Only this contract can initiate flash loan");
+
+        if (msg.sender == address(mainLender)) {
+            MainCallbackData memory data = abi.decode(params, (MainCallbackData));
+            if (data.amountFromFallback > 0) {
+                FallbackCallbackData memory callbackData =
+                    FallbackCallbackData({commonData: data.commonData, mainFee: fee});
+                fallbackLender.flashLoan(this, token, data.amountFromFallback, abi.encode(callbackData));
+            } else {
+                _issueFlashloan(token, data.commonData, fee);
+            }
+        } else if (msg.sender == address(fallbackLender)) {
+            FallbackCallbackData memory data = abi.decode(params, (FallbackCallbackData));
+            _issueFlashloan(token, data.commonData, data.mainFee + fee);
+        } else {
+            revert("Invalid provider");
+        }
+
+        IERC20(token).forceApprove(msg.sender, amount + fee);
+
+        return keccak256("ERC3156FlashBorrower.onFlashLoan");
+    }
+}
---
+++ test/CombinedERC3156Wrapper.t.sol
@@ -0,0 +1,118 @@
+pragma solidity ^0.8.10;
+
+import {PoolTesting, PoolConfigurator} from "@yldr-lending/core/test/libraries/PoolTesting.sol";
+import {BaseTest} from "@yldr-lending/core/test/base/BaseTest.sol";
+import {console2} from "forge-std/console2.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {IPool} from "@yldr-lending/core/src/interfaces/IPool.sol";
+import {AaveERC3156Wrapper, IPool as IAavePool} from "../src/flashloan/AaveERC3156Wrapper.sol";
+import {YLDRERC3156Wrapper} from "../src/flashloan/YLDRERC3156Wrapper.sol";
+import {CombinedERC3156Wrapper} from "../src/flashloan/CombinedERC3156Wrapper.sol";
+import {AssetConverter, IAssetConverter} from "../src/AssetConverter.sol";
+import {UniswapV3Converter, IQuoterV2} from "../src/converters/UniswapV3Converter.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IYLDROracle, IPriceOracleGetter} from "@yldr-lending/core/src/interfaces/IYLDROracle.sol";
+import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
+
+contract CombinedERC3156WrapperTest is BaseTest, IERC3156FlashBorrower {
+    using PoolTesting for PoolTesting.Data;
+    using SafeERC20 for IERC20Metadata;
+
+    IERC20Metadata usdc = IERC20Metadata(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+
+    PoolTesting.Data poolTesting;
+
+    AaveERC3156Wrapper aaveFlashloan;
+    YLDRERC3156Wrapper yldrFlashloan;
+    CombinedERC3156Wrapper combinedFlashloan;
+
+    IPool yldrPool;
+    IAavePool aavePool;
+
+    constructor() {
+        vm.createSelectFork("mainnet");
+        vm.rollFork(18630167);
+
+        _addAndDealToken(usdc);
+
+        vm.startPrank(ADMIN);
+        poolTesting.init(ADMIN, 2);
+
+        poolTesting.addReserve(
+            address(usdc), 0.8e27, 0, 0.02e27, 0.8e27, 0.7e4, 0.75e4, 1.05e4, 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6
+        );
+
+        PoolConfigurator(poolTesting.addressesProvider.getPoolConfigurator()).setReserveFlashLoaning(
+            address(usdc), true
+        );
+
+        yldrPool = IPool(poolTesting.addressesProvider.getPool());
+        aavePool = IAavePool(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2);
+
+        yldrFlashloan = new YLDRERC3156Wrapper(yldrPool);
+        aaveFlashloan = new AaveERC3156Wrapper(aavePool);
+        combinedFlashloan = new CombinedERC3156Wrapper(yldrFlashloan, aaveFlashloan);
+
+        // Supply so the pool has funds for flashloan operations
+        vm.startPrank(BOB);
+        usdc.forceApprove(address(yldrPool), type(uint256).max);
+        yldrPool.supply(address(usdc), 1_000e6, BOB, 0);
+
+        vm.startPrank(ALICE);
+    }
+
+    bytes expectedData;
+    uint256 expectedBalanceAfter;
+    uint256 expectedYLDRBalanceAfter;
+    uint256 expectedAaveBalanceAfter;
+
+    function _getYLDRBalance() internal view returns (uint256) {
+        return usdc.balanceOf(yldrPool.getReserveData(address(usdc)).yTokenAddress);
+    }
+
+    function _getAaveBalance() internal view returns (uint256) {
+        return usdc.balanceOf(aavePool.getReserveData(address(usdc)).aTokenAddress);
+    }
+
+    function _testWithAmount(uint256 amount) internal {
+        expectedData = hex"1234";
+        expectedBalanceAfter = usdc.balanceOf(address(this)) + amount;
+
+        uint256 curYldrBalance = _getYLDRBalance();
+        uint256 curAaveBalance = _getAaveBalance();
+
+        if (curYldrBalance >= amount) {
+            expectedYLDRBalanceAfter = curYldrBalance - amount;
+            expectedAaveBalanceAfter = curAaveBalance;
+        } else {
+            expectedYLDRBalanceAfter = 0;
+            expectedAaveBalanceAfter = curAaveBalance - (amount - curYldrBalance);
+        }
+
+        combinedFlashloan.flashLoan(this, address(usdc), amount, expectedData);
+    }
+
+    function test_can_flashloan() public {
+        _testWithAmount(100e6);
+        _testWithAmount(300e6);
+        _testWithAmount(700e6);
+        _testWithAmount(1000e6);
+        _testWithAmount(10_000e6);
+        _testWithAmount(1_000_000e6);
+    }
+
+    function onFlashLoan(address initiator, address token, uint256 amount, uint256 fee, bytes calldata data)
+        external
+        returns (bytes32)
+    {
+        assertEq(token, address(usdc));
+        assertEq(data, expectedData);
+        assertEq(usdc.balanceOf(address(this)), expectedBalanceAfter);
+        assertEq(_getYLDRBalance(), expectedYLDRBalanceAfter);
+        assertEq(_getAaveBalance(), expectedAaveBalanceAfter);
+        assertEq(initiator, ALICE);
+
+        usdc.forceApprove(msg.sender, amount + fee);
+        return keccak256("ERC3156FlashBorrower.onFlashLoan");
+    }
+}
