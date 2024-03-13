1cf291577e59de8aabe5249821c2a592c5a0aa92
Arsenii Kulikov <klkvrr@gmail.com>
2024-03-13 05:53:42 +0400

    update deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -12,13 +12,16 @@ import {UniswapV3DataProvider} from "../src/ui/UniswapV3DataProvider.sol";
 import {INonfungiblePositionManager} from "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
 import {UniswapV3LeverageDataProvider} from "../src/ui/UniswapV3LeverageDataProvider.sol";
 import {UniswapV3DepositZap} from "../src/UniswapV3DepositZap.sol";
+import {PositionManagerLeverageWrapper} from "../src/leverage/PositionManagerLeverageWrapper.sol";
 
 contract DeployScript is Script {
     function periphery(
         address weth,
         IPoolAddressesProvider addressesProvider,
         IChainlinkAggregatorV3 networkBaseTokenPriceInUsdProxyAggregator,
-        IERC1155UniswapV3Wrapper uniswapV3Wrapper
+        IERC1155UniswapV3Wrapper uniswapV3Wrapper,
+        uint256 leverageFee,
+        address leverageFeeTreasury
     ) public {
         vm.startBroadcast();
 
@@ -28,10 +31,13 @@ contract DeployScript is Script {
         WalletBalanceProvider walletBalancesProvider = new WalletBalanceProvider();
         WETHGateway wETHGateway = new WETHGateway(weth, IPool(addressesProvider.getPool()));
         UniswapV3DataProvider uniswapV3DataProvider = new UniswapV3DataProvider(positionManager);
-        UniswapV3Leverage leverage = new UniswapV3Leverage(addressesProvider, uniswapV3Wrapper, 1000, address(this));
+        UniswapV3Leverage leverage =
+            new UniswapV3Leverage(addressesProvider, uniswapV3Wrapper, leverageFee, leverageFeeTreasury);
         UniswapV3LeverageDataProvider uniswapV3LeverageDataProvider =
             new UniswapV3LeverageDataProvider(uniswapV3DataProvider, leverage);
         UniswapV3DepositZap uniswapV3DepositZap = new UniswapV3DepositZap(addressesProvider, uniswapV3Wrapper);
+        PositionManagerLeverageWrapper positionManagerLeverageWrapper =
+            new PositionManagerLeverageWrapper(positionManager, leverage);
 
         console2.log("UIPoolDataProvider:", address(uIPoolDataProvider));
         console2.log("WalletBalanceProvider:", address(walletBalancesProvider));
@@ -40,5 +46,6 @@ contract DeployScript is Script {
         console2.log("UniswapV3Leverage:", address(leverage));
         console2.log("UniswapV3LeverageDataProvider:", address(uniswapV3LeverageDataProvider));
         console2.log("UniswapV3DepositZap:", address(uniswapV3DepositZap));
+        console2.log("PositionManagerLeverageWrapper:", address(positionManagerLeverageWrapper));
     }
 }
